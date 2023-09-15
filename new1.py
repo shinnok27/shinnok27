1d3d9c85598dbd77a229ef4b75abf0645bf61f01
raphael polanco <shinnok@hotmail.com>
2023-09-15 14:02:54 -0400

    Update new2.py

--- new1.py
+++ new1.py
@@ -1,10 +1,10 @@
 from netmiko import ConnectHandler
 
 
-HOST = Ix #host name 
-USER = shinnok #username to access the router 
-PASS = Teresa27272727 #authentication password 
-TYPE = winntX #device type 
+HOST = "Ix" #host name 
+USER = "shinnok" #username to access the router 
+PASS = "Teresa27272727" #authentication password 
+TYPE = "winntX" #device type 
 
 #using netmiko's CopnnectHandler initiate a connection to the device 
 r1 – ConnectHandler (host=HOST, username=USER, password=PASS, device_type=TYPE
